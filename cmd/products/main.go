799b8efeb5d22a169939f3bdcd00798b55359a6e
Lucas S. Vieira <lucasvieira@protonmail.com>
2023-06-06 23:28:43 -0300

    Add templates for gRPC servers

--- cmd/products/main.go
+++ cmd/products/main.go
@@ -13,49 +13,43 @@ import (
 	rpc "minervarpc"
 )
 
-type UserServerImpl struct {
-	rpc.UnimplementedUserServer
+type ProductsServerImpl struct {
+	rpc.UnimplementedProductsServer
 }
 
-func (UserServerImpl) Index(ctx context.Context, idx *rpc.PageIndex) (*rpc.UserList, error) {
-	log.Print("Index method called")
-	log.Printf("Index passed: %s", idx)
+func (ProductsServerImpl) Index(ctx context.Context, idx *rpc.PageIndex) (*rpc.ProductList, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Index not implemented")
 }
 
-func (UserServerImpl) Show(ctx context.Context, idx *rpc.EntityIndex) (*rpc.User, error) {
-	log.Print("Show method called")
+func (ProductsServerImpl) Show(ctx context.Context, idx *rpc.EntityIndex) (*rpc.Product, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Show not implemented")
 }
 
-func (UserServerImpl) Store(ctx context.Context, user *rpc.User) (*rpc.User, error) {
-	log.Print("Store method called")
+func (ProductsServerImpl) Store(ctx context.Context, product *rpc.Product) (*rpc.Product, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Store not implemented")
 }
 
-func (UserServerImpl) Update(ctx context.Context, user *rpc.User) (*rpc.User, error) {
-	log.Print("Update method called")
+func (ProductsServerImpl) Update(ctx context.Context, product *rpc.Product) (*rpc.Product, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Update not implemented")
 }
 
-func (UserServerImpl) Delete(ctx context.Context, idx *rpc.EntityIndex) (*emptypb.Empty, error) {
-	log.Print("Delete method called")
+func (ProductsServerImpl) Delete(ctx context.Context, idx *rpc.EntityIndex) (*emptypb.Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Delete not implemented")
 }
 
-func createServer() *UserServerImpl {
-	s := &UserServerImpl{}
+func createServer() *ProductsServerImpl {
+	s := &ProductsServerImpl{}
 	return s
 }
 
 func main() {
 	log.Print("Hello world!")
-	listener, err := net.Listen("tcp", "0.0.0.0:9010")
+	listener, err := net.Listen("tcp", "0.0.0.0:9012")
 	if err != nil {
 		log.Fatalf("Failed to start gRPC server: %v", err)
 	}
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
-	rpc.RegisterUserServer(grpcServer, createServer())
+	rpc.RegisterProductsServer(grpcServer, createServer())
 	grpcServer.Serve(listener)
 }
---
+++ cmd/session/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	context "context"
+	grpc "google.golang.org/grpc"
+	status "google.golang.org/grpc/status"
+	codes "google.golang.org/grpc/codes"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
+	
+	rpc "minervarpc"
+)
+
+type SessionServerImpl struct {
+	rpc.UnimplementedSessionServer
+}
+
+func (SessionServerImpl) Generate(ctx context.Context, session *rpc.SessionCreationData) (*rpc.SessionToken, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Generate not implemented")
+}
+
+func (SessionServerImpl) Retrieve(ctx context.Context, token *rpc.SessionToken) (*rpc.SessionData, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Retrieve not implemented")
+}
+
+func (SessionServerImpl) Remove(ctx context.Context, token *rpc.SessionToken) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Remove not implemented")
+}
+
+func createServer() *SessionServerImpl {
+	s := &SessionServerImpl{}
+	return s
+}
+
+func main() {
+	log.Print("Hello world!")
+	listener, err := net.Listen("tcp", "0.0.0.0:9011")
+	if err != nil {
+		log.Fatalf("Failed to start gRPC server: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	rpc.RegisterSessionServer(grpcServer, createServer())
+	grpcServer.Serve(listener)
+}
