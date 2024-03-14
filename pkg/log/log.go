94a52d41a66e0416b5dadaa3b0940b6943291353
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-03-14 13:26:51 -0300

    Make logs register callee function data

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -3,6 +3,7 @@ package minervalog
 import (
 	"fmt"
 	"os"
+	"runtime"
 	"log/slog"
 )
 
@@ -27,22 +28,97 @@ func Init() {
 
 
 func Info(format string, args ...any) {
-    slog.Info(fmt.Sprintf(format, args...))
+	pc, file, line, ok := runtime.Caller(1)
+	var fn string
+	if !ok {
+		file = "unknown"
+		line = 0
+		fn = "unknown"
+	} else {
+		fn = runtime.FuncForPC(pc).Name()
+	}
+	
+    slog.Info(
+		fmt.Sprintf(format, args...),
+		"file", file,
+		"line", line,
+		"function", fn,
+	)
 }
 
 func Warn(format string, args ...any) {
-    slog.Warn(fmt.Sprintf(format, args...))
+	pc, file, line, ok := runtime.Caller(1)
+	var fn string
+	if !ok {
+		file = "unknown"
+		line = 0
+		fn = "unknown"
+	} else {
+		fn = runtime.FuncForPC(pc).Name()
+	}
+	
+    slog.Warn(
+		fmt.Sprintf(format, args...),
+		"file", file,
+		"line", line,
+		"function", fn,
+	)
 }
 
 func Debug(format string, args ...any) {
-    slog.Debug(fmt.Sprintf(format, args...))
+	pc, file, line, ok := runtime.Caller(1)
+	var fn string
+	if !ok {
+		file = "unknown"
+		line = 0
+		fn = "unknown"
+	} else {
+		fn = runtime.FuncForPC(pc).Name()
+	}
+	
+    slog.Debug(
+		fmt.Sprintf(format, args...),
+		"file", file,
+		"line", line,
+		"function", fn,
+	)
 }
 
 func Error(format string, args ...any) {
-    slog.Error(fmt.Sprintf(format, args...))
+	pc, file, line, ok := runtime.Caller(1)
+	var fn string
+	if !ok {
+		file = "unknown"
+		line = 0
+		fn = "unknown"
+	} else {
+		fn = runtime.FuncForPC(pc).Name()
+	}
+	
+    slog.Error(
+		fmt.Sprintf(format, args...),
+		"file", file,
+		"line", line,
+		"function", fn,
+	)
 }
 
 func Fatal(format string, args ...any) {
-    slog.Error(fmt.Sprintf(format, args...))
+	pc, file, line, ok := runtime.Caller(1)
+	var fn string
+	if !ok {
+		file = "unknown"
+		line = 0
+		fn = "unknown"
+	} else {
+		fn = runtime.FuncForPC(pc).Name()
+	}
+	
+    slog.Error(
+		fmt.Sprintf(format, args...),
+		"file", file,
+		"line", line,
+		"function", fn,
+	)
 	os.Exit(1)
 }
