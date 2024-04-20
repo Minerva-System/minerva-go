457b276ce3a448375bc077cb8f4af521f0336727
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-04-20 15:03:45 -0300

    Add gRPC TENANT Service pool to REST

--- internal/connection/grpc.go
+++ internal/connection/grpc.go
@@ -1,15 +1,15 @@
 package minerva_connection
 
 import (
-	"os"
-	"fmt"
-	"time"
 	"errors"
-	
+	"fmt"
+	"os"
+	"time"
+
+	grpcpool "github.com/processout/grpc-go-pool"
 	grpc "google.golang.org/grpc"
 	insecure "google.golang.org/grpc/credentials/insecure"
-	grpcpool "github.com/processout/grpc-go-pool"
-	
+
 	log "github.com/Minerva-System/minerva-go/pkg/log"
 )
 
@@ -17,19 +17,20 @@ const (
 	GrpcClientKindUser     string = "USER"
 	GrpcClientKindSession  string = "SESSION"
 	GrpcClientKindProducts string = "PRODUCTS"
+	GrpcClientKindTenant   string = "TENANT"
 )
 
 func newGrpcClientPool(clientKind string) (*grpcpool.Pool, error) {
 	var host string
 	var exists bool
-	
+
 	varname := fmt.Sprintf("MINERVA_%s_HOST", clientKind)
 	log.Debug("Host env variable: %s", varname)
 	if host, exists = os.LookupEnv(varname); !exists {
 		log.Error("Host for %s service not defined", clientKind)
 		return nil, errors.New(fmt.Sprintf("%s not defined", host))
 	}
-	
+
 	var factory grpcpool.Factory = func() (*grpc.ClientConn, error) {
 		conn, err := grpc.Dial(host, grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if err != nil {
@@ -44,6 +45,5 @@ func newGrpcClientPool(clientKind string) (*grpcpool.Pool, error) {
 		return conn, err
 	}
 
- 	return grpcpool.New(factory, 1, 5, time.Second)
+	return grpcpool.New(factory, 1, 5, time.Second)
 }
-
