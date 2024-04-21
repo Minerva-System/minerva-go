97e3d69a8998b174c9a4fdaf19e69bbc432e1e5a
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-04-21 13:55:35 -0300

    Remove numeric tag for price validation

--- internal/schema/products.go
+++ internal/schema/products.go
@@ -10,7 +10,7 @@ import (
 type NewProduct struct {
 	Description string          `json:"description" validate:"required,max=200"`
 	Unit        string          `json:"unit" validate:"required,len=2"`
-	Price       decimal.Decimal `json:"price" validate:"required,numeric"`
+	Price       decimal.Decimal `json:"price" validate:"required"`
 }
 
 func (n *NewProduct) ToMessage(companyId string) rpc.Product {
@@ -25,7 +25,7 @@ func (n *NewProduct) ToMessage(companyId string) rpc.Product {
 type UpdatedProduct struct {
 	Description string          `json:"description" validate:"max=200"`
 	Unit        string          `json:"unit" validate:"len=2"`
-	Price       decimal.Decimal `json:"price" validate:"numeric"`
+	Price       decimal.Decimal `json:"price" validate:""`
 }
 
 func (n *UpdatedProduct) ToMessage(companyId string, id string) rpc.Product {
